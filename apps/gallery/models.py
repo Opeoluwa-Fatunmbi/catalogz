e5a691abdb40cfff2461ccc0462585beefdf2528
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2024-03-29 18:55:28 +0100

    [Add] Gallery Models

--- apps/gallery/models.py
+++ apps/gallery/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.utils.translation import gettext_lazy
+from django.utils.translation import gettext_lazy as _
 from apps.common.models import BaseModel
 from autoslug import AutoSlugField
+from apps.accounts.models import User
 
 # Create your models here.
 
@@ -12,3 +13,6 @@ class Gallery(BaseModel):
     slug = AutoSlugField(populate_from="name", unique=True, always_update=True)
     desc = models.TextField(_("Description"))
     image = models.ImageField(default="fallback.jpg", upload_to="products/")
+
+    def __str__(self):
+        return self.name
