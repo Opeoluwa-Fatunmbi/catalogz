fcee3b4fe26587f70b0b672026dd41a3d621f9f6
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2024-03-29 18:53:17 +0100

    [Add] Gallery Model Fields

--- apps/gallery/models.py
+++ apps/gallery/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.utils.translation import gettext_lazy
-import uuid
 from apps.common.models import BaseModel
+from autoslug import AutoSlugField
 
 # Create your models here.
 
 
 
 class Gallery(BaseModel):
+    name = models.CharField(max_length=100)
+    slug = AutoSlugField(populate_from="name", unique=True, always_update=True)
+    desc = models.TextField(_("Description"))
+    image = models.ImageField(default="fallback.jpg", upload_to="products/")
