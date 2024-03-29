4779080d34f58fc444c2a6891e69e31ebbf1d9cd
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2024-03-29 18:51:06 +0100

    [Add] Gallery Models

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
--- apps/gallery/models.py
+++ apps/gallery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy
+import uuid
+from apps.common.models import BaseModel
 
 # Create your models here.
+
+
+
+class Gallery(BaseModel):
