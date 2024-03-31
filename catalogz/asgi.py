5c71f6ad345237132523bc3a3b57d38ad76309cb
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2024-03-31 22:11:36 +0100

    [Fix] Vercel Settings

--- catalogz/asgi.py
+++ catalogz/asgi.py
@@ -17,3 +17,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"catalogz.settings.{SETTINGS}")
 
 application = get_asgi_application()
+
+app = application
--- catalogz/wsgi.py
+++ catalogz/wsgi.py
@@ -17,3 +17,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"catalogz.settings.{SETTINGS}")
 
 application = get_wsgi_application()
+
+app = application
