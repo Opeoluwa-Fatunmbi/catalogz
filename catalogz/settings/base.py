5154381d41154421f1aa68462dc6406044d5a8c8
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2024-03-31 20:25:13 +0100

    [Fix] Vercel Fix

--- catalogz/settings/base.py
+++ catalogz/settings/base.py
@@ -15,7 +15,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = [".vercel.app"]
 
 # Application definition
 
